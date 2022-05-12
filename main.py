2b7a08a7e5ca8b60a39ded485569fc2a5c0c571c
LukasKalit <lukaskalit@gmail.com>
2022-05-12 19:13:58 +0200

    Change json text

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 from fastapi import FastAPI
 
+
+class HerokuApp:
+    app_url = "https://devsite-daftcode.herokuapp.com/"
+
 app = FastAPI()
+
+# @app.get("/")
+# def root():
+#     return {"msg": "Hello World"}
 
 @app.get("/")
 def root():
-    return {"message": "Hello World"}
+    return {"start": "1970-01-01"}
+
+
+@app.get("/hello/{name}")
+async def read_item(name: str):
+    return f"Hello {name}"
