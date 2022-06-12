6e44354c6d3ae076dc5c8b4a2eb2445816f489a1
LukasKalit <lukaskalit@gmail.com>
2022-06-13 00:58:18 +0200

    small update

--- app/database.py
+++ app/database.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import sessionmaker
 
 # SQLALCHEMY_DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URL")
 
-# SQLALCHEMY_DATABASE_URL='postgresql://rbivywncdtdppf:ec40827c33c1b54f9204bd60182502c96d63b5e104b33406cb32d1f41b59e1bb@ec2-52-30-67-143.eu-west-1.compute.amazonaws.com:5432/db44bsq6ck9o97'
+SQLALCHEMY_DATABASE_URL='postgresql://rbivywncdtdppf:ec40827c33c1b54f9204bd60182502c96d63b5e104b33406cb32d1f41b59e1bb@ec2-52-30-67-143.eu-west-1.compute.amazonaws.com:5432/db44bsq6ck9o97'
 SQLALCHEMY_DATABASE_URL_LOCAL = 'postgresql://postgres:lk1234589@127.0.0.1:5432/Dafttest'
 
 try:
